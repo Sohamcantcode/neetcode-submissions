754f40e08339aec12eaf1ec4c12cf0497df8a2c8
Sohamcantcode <soham.borikar1@gmail.com>
2026-07-08 22:32:36 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,48 @@
+class MinStack {
+public:
+    vector <int> arr;
+    vector <int> minArr;
+    MinStack() 
+    {
+
+    }
+    
+    void push(int val) 
+    {
+        if(arr.size() == 0)
+        {
+            arr.push_back(val);
+            minArr.push_back(val);
+        }
+        else
+        {
+            arr.push_back(val);
+            if(val < minArr.back())
+            {
+                minArr.push_back(val);
+            }
+            else
+            {
+                minArr.push_back(minArr[minArr.size() - 1]);
+            }
+        }
+
+        
+    }
+    
+    void pop() {
+        arr.pop_back();
+        minArr.pop_back();
+        
+    }
+    
+    int top() 
+    {
+        return arr.back();
+    }
+    
+    int getMin() 
+    {
+        return minArr.back();
+    }
+};
